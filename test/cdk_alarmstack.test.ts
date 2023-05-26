c4e304c096fa35d78d3fe68907731b10ab207284
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-05-26 14:59:37 +0100

    Initial commit

---
+++ test/cdk_alarmstack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAlarmstack from '../lib/cdk_alarmstack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_alarmstack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAlarmstack.CdkAlarmstackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
